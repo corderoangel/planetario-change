fd68bf181497b51e78a1d2f5442ba5669763e683
corderoangel <corderoan777@gmail.com>
2025-02-14 18:26:22 -0500

    filtrado

--- src/app/page.js
+++ src/app/page.js
@@ -5,11 +5,19 @@ import { useState, useEffect } from "react";
 export default function Home() {
 	//agregar estado
 	const [planets, setPlanets] = useState([]);
+	//estado para buscar planeta
+	const [searchPlanet, setSearchPlanet] = useState("");
+
+	//función de busqueda
+	const handleSearchChange = (event) => {
+		setSearchPlanet(event.target.value);
+	};
 
 	//agregar efecto (incluir manejo de errores)
 	useEffect(() => {
 		const fetchPlanets = async () => {
 			try {
+				//se hizo configuración de CORS
 				const res = await fetch("/api/planets");
 				const data = await res.json();
 				setPlanets(data || []);
@@ -20,11 +28,15 @@ export default function Home() {
 		fetchPlanets();
 	}, []);
 
-	//retornar solo en caso de que haya resultados (verificar que haya resultados )
+	//filtrar planetas
+	const filteredPlanets = planets.filter((planet) => planet.name.toLowerCase().includes(searchPlanet.toLocaleLowerCase()));
+
+	//actualizamos el retorno para que sea por filtro
 	return (
 		<div>
 			<h1>Resultados</h1>
-			<ul>{planets.length > 0 ? planets.map((e) => <li key={e.name}>{e.name}</li>) : <p>Cargando planetas...</p>}</ul>
+			<input type="text" placeholder="Buscar planeta" value={searchPlanet} onChange={handleSearchChange} />
+			<ul>{filteredPlanets.length > 0 ? filteredPlanets.map((e, index) => <li key={index}>{e.name}</li>) : <p>No se encontraron planetas</p>}</ul>
 		</div>
 	);
 }
